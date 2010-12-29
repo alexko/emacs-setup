4d7277f8a3ad61ecac1bd0598b82eb1a05f04b23
alexko <alex@3form.com>
2010-12-28 20:51:10 -0800

    interactive func improvements: C-u allows to specify proxy, if region is active it is eval in tt-repl

--- elpa-to-submit/tt_lua.el
+++ elpa-to-submit/tt_lua.el
@@ -25,34 +25,52 @@
   :type 'string
   :group 'tt-lua)
 
-(defun tt-lua (spec)
-  ;; todo: override the default proxy on C-u?
+(defun tt-lua (spec proxy expr)
   "Interactive Lua repl for ttserver"
-  (interactive (let ((host-port (read-from-minibuffer (concat "Host:port (" tt-default-thost ":" (int-to-string tt-default-tport) ") "))))
-                 (list host-port)))
-  (defun mapper (x) (cond ((equal x "") nil) ((string-match "^[0-9]+$" x) (string-to-number x)) (t x)))
-  (apply 'tt-repl (mapcar 'mapper (split-string spec ":"))))
+  (interactive
+   (let* ((host-port-prompt
+           (format "Host:port (%s:%d) " tt-default-thost tt-default-tport))
+          (host-port
+           (read-from-minibuffer host-port-prompt))
+          (proxy-cmd
+           (if current-prefix-arg
+               (read-from-minibuffer "Proxy command: " "") tt-proxy-cmd))
+          (expr
+           (if (region-active-p)
+               (buffer-substring-no-properties (region-beginning) (region-end)))))
+     (list host-port proxy-cmd expr)))
+  (defun extract (n lst &optional rest)
+    (if (= n 0) rest
+      (cons (car lst) (extract (- n 1) (cdr lst) rest))))
+  (defun mapper (x)
+    (cond ((equal x "") nil)
+          ((and (stringp x) (string-match "^[0-9]+$" x)) (string-to-number x))
+          (t x)))
+  (apply 'tt-repl
+         (extract 3 (mapcar 'mapper (split-string spec ":")) (list proxy expr))))
 
-(defun tt-repl (&optional thost tport rport lcmd)
+(defun tt-repl (&optional thost tport rport proxy-cmd expr)
   "Lua repl for ttserver"
-  (let* (
-         (thost (or thost tt-default-thost))
+  (let* ((thost (or thost tt-default-thost))
          (stport (int-to-string (or tport tt-default-tport)))
-         (srport (int-to-string (or rport tt-default-rport))) ; this won't work if tport is 1999
-         (proxy (split-string tt-proxy-cmd))
-         (tcr (append proxy (list "tcrmgr" "ext" "-port" stport thost "repl" srport)))
-         (cmd (append proxy (list "nc" "-q5" thost srport)))
+         (srport (int-to-string (or rport tt-default-rport)))
+         (proxyl (split-string (or proxy-cmd tt-proxy-cmd)))
+         (tcr (append proxyl (list "tcrmgr" "ext" "-port" stport thost "repl" srport)))
+         (cmd (append proxyl (list "nc" "-q5" thost srport)))
          (pname (concat thost ":" stport))
          (wname (concat "*" pname "*"))
-         (tproc (apply 'start-process " ttserver" "ttserver" tcr)))
+         (tproc (apply 'start-process " ttserver" "ttserver" tcr))
+         (exprn (if expr (concat expr "\n") nil)))
     (sleep-for 0 500)
-    (if (equal 'run (process-status tproc))
-        (progn
-          (delete-other-windows)
-          (switch-to-buffer-other-window wname)
-          (insert (concat "Lua repl at " pname "\n"))
-          (apply 'make-comint pname (car cmd) nil (cdr cmd))
-          (if lcmd (comint-send-string
-                    (get-buffer-process wname) (concat lcmd "\n")))))))
+    (when (equal 'run (process-status tproc))
+      (delete-other-windows)
+      (switch-to-buffer-other-window wname)
+      (insert (format "TT Lua repl %s %s\n" pname
+                      (if proxyl (combine-and-quote-strings (cons "via" proxyl)) "")))
+      (if exprn (insert exprn))
+      (apply 'make-comint pname (car cmd) nil (cdr cmd))
+      (when exprn
+        (comint-send-string
+         (get-buffer-process wname) exprn)))))
 
 (provide 'tt-lua)
