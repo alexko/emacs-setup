d26699b50da1475d4b27a342013ada83939db855
alexko <alex@3form.com>
2010-12-29 14:13:16 -0800

    adds custom org-remember templates instead of overwriting the var

--- alex-custom.el
+++ alex-custom.el
@@ -31,7 +31,9 @@
 ;;         ("Journal" ?j "* %U %?\n\n  %i\n  %a" "~/org/journal.org")
 ;;         ("Idea" ?i "* %?\n  %i\n  %a" "~/org/journal.org" "Ideas")))
 (setq org-remember-templates
-      '(("Todo" ?t "* TODO %^{Title} %^g\n%?\n  %i\n  %a\nAdded: %U" "~/org/tasks.org" "Tasks")
+      (append
+       (if (boundp 'org-remember-templates) org-remember-templates nil)
+       '(("Todo" ?t "* TODO %^{Title} %^g\n%?\n  %i\n  %a\nAdded: %U" "~/org/tasks.org" "Tasks")
         ("Journal"   ?j "* %^{Title} %U %^g\n%?\n  %i\n  %a" "~/org/journal.org")
         ("Clip" ?x "* %^{Title} %U :xclip:\n%?\n  %x\n  %a" "~/org/journal.org")
         ("Clip" ?y "* %^{Title} %U :yclip:\n%?\n  %c\n  %a" "~/org/journal.org")
@@ -41,7 +43,7 @@
         ("Review" ?a "* Daily review %T :review: \n%[~/.emacs.d/org/.review.tmpl]\n"
          "~/org/journal.org")
         ("Idea" ?i "* %^{Title} %^g\n%?\n  %i\n  %x\n  %a\nAdded: %U"
-         "~/org/journal.org" "Ideas")))
+         "~/org/journal.org" "Ideas"))))
 
 (define-key global-map "\C-cr" 'org-remember)
 ;(define-key global-map "\C-cc" 'org-capture)
