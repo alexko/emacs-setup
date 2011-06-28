e09b051930495bce35073c94d1bf0f57244ac3fe
alexko <alex@3form.com>
2011-06-27 21:08:34 -0700

    removing org-remember (superceded by org-capture)

--- alex-custom.el
+++ alex-custom.el
@@ -67,31 +67,9 @@
 
 (setq org-attach-directory (concat org-directory "/data"))
 (setq org-agenda-files (list org-directory))
-(setq remember-annotation-functions '(org-remember-annotation))
-(setq remember-handler-functions '(org-remember-handler)) ;!!
-(add-hook 'remember-mode-hook 'org-remember-apply-template)
-;; (setq org-remember-templates
-;;       '(("Todo" ?t "* TODO %?\n  %i\n  %a" "~/org/todo.org" "Tasks")
-;;         ("Journal" ?j "* %U %?\n\n  %i\n  %a" "~/org/journal.org")
-;;         ("Idea" ?i "* %?\n  %i\n  %a" "~/org/journal.org" "Ideas")))
-(setq org-remember-templates
-      (append
-       (if (boundp 'org-remember-templates) org-remember-templates nil)
-       '(("Todo" ?t "* TODO %^{Title} %^g\n%?\n  %i\n  %a\nAdded: %U" "~/org/tasks.org" "Tasks")
-        ("Journal"   ?j "* %^{Title} %U %^g\n%?\n  %i\n  %a" "~/org/journal.org")
-        ("Clip" ?x "* %^{Title} %U :xclip:\n%?\n  %x\n  %a" "~/org/journal.org")
-        ("Clip" ?y "* %^{Title} %U :yclip:\n%?\n  %c\n  %a" "~/org/journal.org")
-        ("Expenses"   ?e "* %^{Title} %U %^g\n%?"   "~/org/finance.org")
-        ("Book" ?b "* %^{Title} %t :book: \n%[~/.emacs.d/org/.book.tmpl]\n" 
-         "~/org/journal.org")
-        ("Review" ?a "* Daily review %T :review: \n%[~/.emacs.d/org/.review.tmpl]\n"
-         "~/org/journal.org")
-        ("Idea" ?i "* %^{Title} %^g\n%?\n  %i\n  %x\n  %a\nAdded: %U"
-         "~/org/journal.org" "Ideas"))))
 
 (define-key global-map "\C-cb" 'org-ido-switchb)
 (global-set-key (kbd "C-<f11>") 'org-clock-in)
-(define-key global-map "\C-cr" 'org-remember)
 
 
 (define-key global-map "\C-cc" 'org-capture)
