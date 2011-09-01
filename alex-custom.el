3035e32bc08ad066afc076d6438beeffab5ad97e
alexko <alex@3form.com>
2011-09-01 16:33:48 -0700

    adjust effort props

--- alex-custom.el
+++ alex-custom.el
@@ -69,6 +69,11 @@
 (setq org-attach-directory (concat org-directory "/data"))
 (setq org-agenda-files (list org-directory))
 
+(setq org-sort-agenda-noeffort-is-high nil)
+(setq org-global-properties
+      '(("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00")
+        ("COLUMNS". "%30ITEM %8TAGS %PRIORITY %TODO %5Effort{:} %5CLOCKSUM")))
+
 (define-key global-map "\C-cb" 'org-ido-switchb)
 (global-set-key (kbd "C-<f11>") 'org-clock-in)
 
