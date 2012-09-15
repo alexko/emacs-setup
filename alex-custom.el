217745ded1382f353c63b3346f73a132db155985
alexko <alex@3form.com>
2012-09-15 14:45:28 -0700

    more usable setting for the effort property

--- alex-custom.el
+++ alex-custom.el
@@ -95,7 +95,7 @@
 
 (setq org-sort-agenda-noeffort-is-high nil)
 (setq org-global-properties
-      '(("Effort_ALL". "0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00")
+      '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
         ("COLUMNS". "%30ITEM %8TAGS %PRIORITY %TODO %5Effort{:} %5CLOCKSUM")))
 
 (define-key global-map "\C-cb" 'org-ido-switchb)
