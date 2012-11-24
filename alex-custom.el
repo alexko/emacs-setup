8767290f24b66441e71f8176ad2ae9bad74e8f5f
alexko <alex@3form.com>
2012-11-23 17:31:21 -0800

    update org-column settings

--- alex-custom.el
+++ alex-custom.el
@@ -111,7 +111,7 @@
 (setq org-sort-agenda-noeffort-is-high nil)
 (setq org-global-properties
       '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
-        ("COLUMNS". "%30ITEM %8TAGS %PRIORITY %TODO %5Effort{:} %5CLOCKSUM")))
+        ("COLUMNS". "%30ITEM %PRIORITY %DEADLINE %SCHEDULED %5Effort{:} %5CLOCKSUM")))
 
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cb" 'org-ido-switchb)
