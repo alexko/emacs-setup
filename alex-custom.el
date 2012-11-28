5b4fa83707a607c500ce38f13e141b9574353c5c
alexko <alex@3form.com>
2012-11-27 21:50:41 -0800

    specify location

--- alex-custom.el
+++ alex-custom.el
@@ -105,6 +105,8 @@
 (setq org-global-properties
       '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
         ("COLUMNS". "%30ITEM %PRIORITY %DEADLINE %SCHEDULED %5Effort{:} %5CLOCKSUM")))
+(setq calendar-latitude 38)
+(setq calendar-longitude -122)
 
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cb" 'org-ido-switchb)
