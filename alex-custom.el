c4b0e695ae706bbc5704d565c703fde7bca20b7f
alexko <alex@3form.com>
2014-03-10 23:29:58 -0700

    set org-clock-idle-time

--- alex-custom.el
+++ alex-custom.el
@@ -117,6 +117,8 @@
 (setq org-global-properties
       '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
         ("COLUMNS". "%30ITEM %PRIORITY %DEADLINE %SCHEDULED %5Effort{:} %5CLOCKSUM")))
+;; (setq org-global-properties nil)
+(setq org-clock-idle-time 10)
 (setq calendar-latitude 38)
 (setq calendar-longitude -122)
 
