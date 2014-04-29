b5a3b6fd41248b10a1cd304e04f730523a77beb3
Alex Kosorukoff <alex@3form.com>
2014-04-28 22:44:15 -0700

    update org-columns setup

--- alex-custom.el
+++ alex-custom.el
@@ -111,7 +111,7 @@
     (setq org-sort-agenda-noeffort-is-high nil)
     (setq org-global-properties
           '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
-            ("COLUMNS". "%30ITEM %PRIORITY %DEADLINE %SCHEDULED %5Effort{:} %5CLOCKSUM")))
+            ("COLUMNS". "%48ITEM %PRIORITY %DEADLINE %SCHEDULED %7Effort{:} %5CLOCKSUM")))
     ;; (setq org-global-properties nil)
     (setq org-clock-idle-time 10)
     (setq calendar-latitude 38)
