2b15a6318d7ba28026eb01a516da975e4be93708
Alex Kosorukoff <alex@3form.com>
2014-05-04 12:05:51 -0700

    fix bug in function getting PATH from shell & remove trailing ws
    it was putting "stty" and " standard input " into path

--- tim-custom.el
+++ tim-custom.el
@@ -1,36 +1,37 @@
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-;;(eval-after-load "pymacs"
-;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+(use-package pymacs
+  :load-path "vendor/"
+  :init (progn
+          (autoload 'pymacs-apply "pymacs")
+          (autoload 'pymacs-call "pymacs"))
+  :commands (pymacs-eval pymacs-exec pymacs-load))
 
-(add-to-list 'load-path "~/.emacs.d/vendor/mmm-mode")
-(require 'mmm-mako)
+(use-package mmm-mako
+  :load-path "vendor/mmm-mode"
+  :mode ("\\.mako\\'" . html-mode)
+  :config
+  (mmm-add-mode-ext-class 'html-mode "\\.mako\\'" 'mako))
 
-(add-to-list 'auto-mode-alist '("\\.mako\\'" . html-mode))
-(mmm-add-mode-ext-class 'html-mode "\\.mako\\'" 'mako)
+(use-package gccsense)
 
-(require 'gccsense)
+(use-package google-c-style
+  :config
+  (progn
+    (add-hook 'c-mode-common-hook 'google-set-c-style)
+    (add-hook 'c-mode-common-hook 'google-make-newline-indent)))
 
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
-
-(defun set-exec-path-from-shell-PATH () 
-  (let ((path-from-shell
-         (shell-command-to-string "$SHELL -i -c 'echo $PATH'"))) 
-    (setenv "PATH" path-from-shell) 
-    (setq exec-path (split-string path-from-shell path-separator)))) 
-
-(when (equal system-type 'darwin) 
-  ;; When started from Emacs.app or similar, ensure $PATH 
-  ;; is the same the user would see in Terminal.app 
-  (setq rinari-tags-file-name "TAGS")
-  (if window-system (set-exec-path-from-shell-PATH))) 
+(when (equal system-type 'darwin)
+  ;; When started from Emacs.app or similar, ensure $PATH
+  ;; is the same the user would see in Terminal.app
+  (defun path-from-shell-PATH ()
+    (let ((path-from-shell
+           (shell-command-to-string "$SHELL -c 'echo $PATH'")))
+      (split-string path-from-shell path-separator)))
+  (when window-system
+      (setenv "PATH" path-from-shell)
+      (setq exec-path (path-from-shell-PATH))))
 
 (setq tab-width 2)
+(setq rinari-tags-file-name "TAGS")
 (setq-default indent-tabs-mode nil)
 (setq-default py-indent-offset 2)
 (provide 'tim-custom)
