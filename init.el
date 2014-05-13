11358f140e69372a458e7251a9d3281463b0cd77
Alex Kosorukoff <alex@3form.com>
2014-05-13 13:10:29 -0700

    remove redundant message about loading user files

--- init.el
+++ init.el
@@ -352,7 +352,6 @@
 (when (file-exists-p user-specific-config)
   (add-to-list 'load-path user-specific-config)
   (dolist (file (directory-files user-specific-config 'noerror ".*el$"))
-    (message "Loading %s" file)
     (or (load (concat file "c") 'noerror) (load file))))
 
 (use-package desktop
