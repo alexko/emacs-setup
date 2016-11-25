21c503e1a1ad95ec9031c8e6ec05223192c0c34d
Alex Kosorukoff <alex@3form.com>
2016-11-25 14:46:01 -0800

    decrease ITEM width by 10 in the column view

--- org-custom.el
+++ org-custom.el
@@ -40,7 +40,7 @@
 
     (setq org-global-properties
           '(("Effort_ALL". "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 0:30")
-            ("COLUMNS". "%48ITEM %PRIORITY %DEADLINE %SCHEDULED %7Effort{:} %5CLOCKSUM")))
+            ("COLUMNS". "%38ITEM %PRIORITY %DEADLINE %SCHEDULED %7Effort{:} %5CLOCKSUM")))
 
     (setq org-special-ctrl-k t)
     (setq org-special-ctrl-a/e t)
